04246609603df28c69009680f4da1a074c137e9c
Charlie Russo <cparusso@gmail.com>
2018-07-27 21:09:02 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,7 +9,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array << element 
+  array << element
 end
 
 def add_element_to_start_of_array(array, element)
@@ -17,7 +17,7 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-
+  element.pop()
 end
 
 def remove_element_from_start_of_array(array)
